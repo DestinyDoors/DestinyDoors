fb5aab0fefe6d761662eac151f57777780f7dc55
Destiny Doors <destinydoors2@gmail.com>
2022-06-05 21:43:19 +0530

    admin updated

--- Destiny Doors/Destiny/home/models.py
+++ Destiny Doors/Destiny/home/models.py
@@ -8,10 +8,10 @@ class newboarn(models.Model):
     id = models.AutoField
     child_name = models.CharField(max_length=150, null=False, blank=False)
     image = models.ImageField(upload_to='upload_pic/images', default="")
-    taught = models.TextField(max_length=200, null=False , blank=False)
-    age = models.CharField(max_length=15, null=False, blank=False)
-    Hobby =  models.TextField(max_length=200, null=True , blank=True)
-    description =  models.TextField(max_length=800, null=False , blank=False)
+    taught = models.TextField(max_length=200, blank=True)
+    age = models.CharField(max_length=15)
+    Hobby =  models.TextField(max_length=200, blank=True)
+    description =  models.TextField(max_length=800, blank=True)
 
     def __str__(self):
         return self.child_name
@@ -20,10 +20,10 @@ class age_3_5y(models.Model):
     id = models.AutoField
     child_name = models.CharField(max_length=150, null=False, blank=False)
     image = models.ImageField(upload_to='upload_pic/images', default="")
-    taught = models.TextField(max_length=200, null=False , blank=False)
-    age = models.CharField(max_length=15, null=False, blank=False)
-    Hobby =  models.TextField(max_length=200, null=False , blank=False)
-    description =  models.TextField(max_length=800, null=False , blank=False)
+    taught = models.TextField(max_length=200, blank=True)
+    age = models.CharField(max_length=15)
+    Hobby =  models.TextField(max_length=200, blank=True)
+    description =  models.TextField(max_length=800, blank=True)
 
     def __str__(self):
         return self.child_name
@@ -32,10 +32,10 @@ class age_6_10y(models.Model):
     id = models.AutoField
     child_name = models.CharField(max_length=150, null=False, blank=False)
     image = models.ImageField(upload_to='upload_pic/images', default="")
-    taught = models.TextField(max_length=200, null=False , blank=False)
-    age = models.CharField(max_length=15, null=False, blank=False)
-    Hobby =  models.TextField(max_length=200, null=False , blank=False)
-    description =  models.TextField(max_length=800, null=False , blank=False)
+    taught = models.TextField(max_length=200, blank=True)
+    age = models.CharField(max_length=15)
+    Hobby =  models.TextField(max_length=200, blank=True)
+    description =  models.TextField(max_length=800, blank=True)
 
     def __str__(self):
         return self.child_name
@@ -44,10 +44,10 @@ class age_11_15y(models.Model):
     id = models.AutoField
     child_name = models.CharField(max_length=150, null=False, blank=False)
     image = models.ImageField(upload_to='upload_pic/images', default="")
-    taught = models.TextField(max_length=200, null=False , blank=False)
-    age = models.CharField(max_length=15, null=False, blank=False)
-    Hobby =  models.TextField(max_length=200, null=False , blank=False)
-    description =  models.TextField(max_length=800, null=False , blank=False)
+    taught = models.TextField(max_length=200, blank=True)
+    age = models.CharField(max_length=15)
+    Hobby =  models.TextField(max_length=200, blank=True)
+    description =  models.TextField(max_length=800, blank=True)
 
     def __str__(self):
         return self.child_name
@@ -56,10 +56,10 @@ class age_16_18y(models.Model):
     id = models.AutoField
     child_name = models.CharField(max_length=150, null=False, blank=False)
     image = models.ImageField(upload_to='upload_pic/images', default="")
-    taught = models.TextField(max_length=200, null=False , blank=False)
-    age = models.CharField(max_length=15, null=False, blank=False)
-    Hobby =  models.TextField(max_length=200, null=False , blank=False)
-    description =  models.TextField(max_length=800, null=False , blank=False)
+    taught = models.TextField(max_length=200, blank=True)
+    age = models.CharField(max_length=15)
+    Hobby =  models.TextField(max_length=200, blank=True)
+    description =  models.TextField(max_length=800, blank=True)
 
     def __str__(self):
         return self.child_name
