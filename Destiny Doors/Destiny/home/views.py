12e29cd3c176ef939c287e164b26d62fe5ecef06
manishmac-dev <mkrg102@gmail.com>
2022-06-08 08:13:19 +0530

    new

--- Destiny Doors/Destiny/home/views.py
+++ Destiny Doors/Destiny/home/views.py
@@ -12,7 +12,6 @@ def contact(request):
     return render(request, 'contact.html')
 def child(request):
     newboarns = newboarn.objects.all()
-    print(newboarns)
     n = len(newboarns)
     nSlides = n//4 + ceil((n/4)-(n//4))
     params = {'no_of_slides':nSlides, 'range': range(nSlides,1),'newboarn': newboarns}
@@ -23,28 +22,27 @@ def adopt(request):
     return render(request, 'adopt.html')
 def age_3_5(request):
     age_3_5ys = age_3_5y.objects.all()
-    print(age_3_5ys)
     n = len(age_3_5ys)
     nSlides = n//4 + ceil((n/4)-(n//4))
     params = {'no_of_slides':nSlides, 'range': range(nSlides,1),'age_3_5y': age_3_5ys}
     return render(request, 'age_3_5.html', params)
 def age_6_10(request):
     age_6_10ys = age_6_10y.objects.all()
-    print(age_6_10ys)
+
     n = len(age_6_10ys)
     nSlides = n//4 + ceil((n/4)-(n//4))
     params = {'no_of_slides':nSlides, 'range': range(nSlides,1),'age_6_10y': age_6_10ys}
     return render(request, 'age_6_10.html', params)
 def age_11_15(request):
     age_11_15ys = age_11_15y.objects.all()
-    print(age_11_15ys)
+    
     n = len(age_11_15ys)
     nSlides = n//4 + ceil((n/4)-(n//4))
     params = {'no_of_slides':nSlides, 'range': range(nSlides,1),'age_11_15y': age_11_15ys}
     return render(request, 'age_11_15.html', params)
 def age_16_18(request):
     age_16_18ys = age_16_18y.objects.all()
-    print(age_16_18ys)
+    
     n = len(age_16_18ys)
     nSlides = n//4 + ceil((n/4)-(n//4))
     params = {'no_of_slides':nSlides, 'range': range(nSlides,1),'age_16_18y': age_16_18ys}
