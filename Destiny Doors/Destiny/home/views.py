e2464298ff51c8cc1d455388bc03f7bff7257ef6
manishmac-dev <mkrg102@gmail.com>
2022-06-08 20:53:30 +0530

    wow

--- Destiny Doors/Destiny/home/views.py
+++ Destiny Doors/Destiny/home/views.py
@@ -10,14 +10,14 @@ def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
+def donation(request):
+    return render(request, 'donation.html')
 def child(request):
     newboarns = newboarn.objects.all()
     n = len(newboarns)
     nSlides = n//4 + ceil((n/4)-(n//4))
     params = {'no_of_slides':nSlides, 'range': range(nSlides,1),'newboarn': newboarns}
     return render(request, 'meetchildren.html', params)
-def donation(request):
-    return render(request, 'donation.html')
 def adopt(request):
     return render(request, 'adopt.html')
 def age_3_5(request):
@@ -47,3 +47,35 @@ def age_16_18(request):
     nSlides = n//4 + ceil((n/4)-(n//4))
     params = {'no_of_slides':nSlides, 'range': range(nSlides,1),'age_16_18y': age_16_18ys}
     return render(request, 'age_16_18.html', params)
+
+
+
+def child_data(request, myid):
+    #fetch product by id
+    newboarns = newboarn.objects.filter(id=myid)
+    print(newboarns)
+    return render(request, 'childviewsite.html', {'newboarn':newboarns[0]})
+
+def age_3_5yr(request, myid):
+    #fetch product by id
+    age_3_5ys = age_3_5y.objects.filter(id=myid)
+    print(age_3_5ys)
+    return render(request, 'age_3_5_viewsite.html', {'age_3_5y':age_3_5ys[0]})
+
+def age_6_10yr(request, myid):
+    #fetch product by id
+    age_6_10ys = age_6_10y.objects.filter(id=myid)
+    print(age_6_10ys)
+    return render(request, 'age_6_10_viewsite.html', {'age_6_10y':age_6_10ys[0]})
+
+def age_11_15yr(request, myid):
+    #fetch product by id
+    age_11_15ys = age_11_15y.objects.filter(id=myid)
+    print(age_11_15ys)
+    return render(request, 'age_11_15_viewsite.html', {'age_11_15y':age_11_15ys[0]})
+
+def age_16_18yr(request, myid):
+    #fetch product by id
+    age_16_18ys = age_16_18y.objects.filter(id=myid)
+    print(age_16_18ys)
+    return render(request, 'age_16_18_viewsite.html', {'age_16_18y':age_16_18ys[0]})
