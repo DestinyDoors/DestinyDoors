ef98c7df07ed8f9449d339104a69c0f4189e80d1
manishmac-dev <mkrg102@gmail.com>
2022-06-19 21:42:07 +0530

    mac

--- Destiny Doors/Destiny/Destiny/settings.py
+++ Destiny Doors/Destiny/Destiny/settings.py
@@ -141,5 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'destinydoors2@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'Destinydoors@mmnka12345'
 EMAIL_PORT = 587
+EMAIL_USE_SSL = False
--- Destiny Doors/Destiny/home/views.py
+++ Destiny Doors/Destiny/home/views.py
@@ -24,6 +24,19 @@ def contact(request):
         message = request.POST.get('message')
         DB=contactme(First_name=first_name,Last_name=last_name,Email_Id=email,Phone=phone,Message=message)
         DB.save()
+
+        data ={
+            'name' : first_name,
+            'email' : email,
+            'subject' : message,
+            'message' : message
+        }
+        message = '''
+        New message" {}
+
+        From: {}
+        '''.format(data['message'], data['email'], )
+        send_mail(data['subject'], message, '',['destinydoors2@gmail.com'])
         
     return render(request, 'contact.html')
 
